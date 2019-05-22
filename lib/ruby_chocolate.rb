5b57fe575c9f5bde9d01c6e50f27b0591b153051
jinroq <2787780+jinroq@users.noreply.github.com>
2019-05-23 01:40:48 +0900

    Change the target of monkey patch from Object#puts to IO#write

--- lib/ruby_chocolate.rb
+++ lib/ruby_chocolate.rb
@@ -3,7 +3,7 @@ require "ruby_chocolate/version"
 module RubyChocolate
   class Error < StandardError; end
 
-  def puts(*args)
-    super("\e[35m#{args.join}\e[45m\e[0m")
+  def write(*str)
+    super("\e[35m#{str.join}\e[45m\e[0m")
   end
 end

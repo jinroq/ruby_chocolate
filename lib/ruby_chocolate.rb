aef0e0532498af64d5ef1b9fa1e7ea6467a2583e
jinroq <2787780+jinroq@users.noreply.github.com>
2019-05-22 12:49:52 +0900

    Modified from using open class to using prepend.

--- lib/ruby_chocolate.rb
+++ lib/ruby_chocolate.rb
@@ -3,11 +3,7 @@ require "ruby_chocolate/version"
 module RubyChocolate
   class Error < StandardError; end
 
-  class Object
-    alias :__puts__ :puts
-    def puts(*args)
-      __puts__("\e[35m#{args.join}\e[45m\e[0m")
-    end
-    private :__puts__
+  def puts(*args)
+    super("\e[35m#{args.join}\e[45m\e[0m")
   end
 end
